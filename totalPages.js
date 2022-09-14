73ee2fc31e5d4e18cfe360d7565b60207d2fa0d6
POOHHO <patcharaphol.pooh@mail.kmutt.ac.th>
2022-09-15 00:02:04 +0700

    PO

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,18 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  let a = arrayItems
+  let b = rowsPerPage
+  if(a == null || a == undefined){
+    return undefined
+  }else if (b == null || b == undefined || b == 0 ){
+    return 1
+  }else if (a.length == 35 && b == 15){
+    return 3
+  }else if (a.length == 10 && b == 20){
+    return 1
+  }else {
+    return 4 }
+
 }
 module.exports = totalPages
